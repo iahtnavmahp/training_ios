66df0c90240e10d95716a1f46d75741764437640
iahtnavmahp <thaisaker114@gmail.com>
2022-03-15 08:33:00 +0700

    test

---
+++ training_ios/training_ios/SaveData/SaveDataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SaveDataViewController.swift
+//  training_ios
+//
+//  Created by Pham Van Thai on 15/03/2022.
+//
+
+import UIKit
+
+class SaveDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
